3720e506282e8c357fe8078f56eafc0b665c99eb
Wil Gunter <in.wil.i.trust@gmailcom>
2013-04-11 01:02:19 -0700

    Setup more core.

--- jparse.js
+++ jparse.js
@@ -1,53 +1,105 @@
 
 var year = $('span.calyeartitle').text();
 var work = $('td.calendarCellRegularPast:first').text();
-console.log($('td.calendarCellRegularPast').text());
-console.log($('td.calendarCellRegularPast').nextUntil('td.calendarCellRegularCurrent').text());
-console.log($('td.calendarCellRegularCurrent').text());
-console.log($('td.calendarCellRegularFuture').text());
-console.log($('td.calendarCellRegularFuture').nextAll().text());
-	
+
+var firstDate = 00;	
 var month = 0;
+var parse = null;
+var parseArray = [];
+var daysPerMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function Date(subject,startDate, startTime, endDate, endTime,allDayEvent,description,location,boolPrivate){
+	this.subject = subject;
+	this.startDate = startDate;
+	this.startTime = startTime;
+	this.endDate = endDate;
+	this.endTime = endTime;
+	this.allDayEvent = allDayEvent;
+	this.description = description;
+	this.location = location;
+	this.boolPrivate = boolPrivate;
+}
+
+var formatDates = function(){
+	/* This is something i will not be able to do without the website being up
+	the plan will be to grab things peice by peice and sort them into objects.
+	will want to create the object via day date for now( may cause problems when
+	we get splitting to second month.*/
+}
+
+var toArray = function (){
+	try{
+		for(int i = 0; i < 14; i++){
+		//tlc down now so all of this is made up ... 0's are the length from date i.e. (07 08:00am to 04:00pm 00359xxxxxxxx)
+			parseArray[i] = parse.substring((i * (00 0000 0 0000 00 000 00000000).length), (00 0000 0 0000 00 000 00000000).length);
+		}
+	}catch(err){
+		console.log("Error in creating array, please check for accuracy");
+	}
+}
+
+var initDates = function(){
+	var parsePast = ($('td.calendarCellRegularPast').text()).replace(/\s+/g,"");
+	var parseCurrent = ($('td.calendarCellRegularCurrent').text()).replace(/\s+/g,"");
+	var parseFuture = ($('td.calendarCellRegularFuture').text()).replace(/\s+/g,"");
+	
+	if(firstDate === 00 && tempParsePast != null){
+		firstDate = tempParsePast.substring(0,2);
+		if(daysPerMonth[month] < (firstDate + 13)){
+			console.log("Next month needs to be loaded");
+		}
+		parse = parsePast + parseCurrent + parseFuture;
+	}else{
+		firstDate = tempParseCurrent.substring(0,2);		
+		if(daysPerMonth[month] < (firstDate + 13)){
+			console.log("Next month needs to be loaded");
+		}
+		parse = parseCurrent + parseFuture;
+	};
+}
 
 var getMonth = function(){
 	switch($('span.calMonthTitle').text()){
-		case January:
+		case "January":
 			month = 1;
 			break;
-		case February:
+		case "February":
 			month = 2;
 			break;
-		case March:
+		case "March":
 			month = 3;
 			break;
-		case April:
+		case "April":
 			month = 4;
 			break;
-		case May:
+		case "May":
 			month = 5;
 			break;
-		case June:
+		case "June":
 			month = 6;
 			break;
-		case July:
+		case "July":
 			month = 7;
 			break;
-		case August:
+		case "August":
 			month = 8;
 			break;
-		case September:
+		case "September":
 			month = 9;
 			break;
-		case October:
+		case "October":
 			month = 10;
 			break;
-		case November:
+		case "November":
 			month = 11;
 			break;
-		case December:
+		case "December":
 			month = 12;
 			break;
 		default:
 			break;
-	}
+	};
 }
+
+getMonth();
+initDates();
