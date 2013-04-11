63ebd1ddb89e753a5133ac2f836325141fae2693
Wil Gunter <in.wil.i.trust@gmailcom>
2013-04-10 21:09:22 -0700

    Add method to grab data from website

--- jparse.js
+++ jparse.js
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 alert("I am an alert box!");
 
 //I was looking at RegExp a little bit and trying
 //to figure out how to search for the dates.
 //This is what I could come up with for it.
 var testDate = /[0-9][0-9]\W[0-9][0-9]\s[AMP]/g;
+=======
+var data = $('span.calMonthTitle').html();
+alert("The month is " + data);
+console.log(data);
+>>>>>>> Add method to grab data from HTML
