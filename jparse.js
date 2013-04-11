bc11a2d0e1b529a886990e52385fa4d4e6b32eb0
David King <dxking16@gmail.com>
2013-04-10 18:28:56 -0700

    attempt at looking for a date

--- jparse.js
+++ jparse.js
@@ -1,1 +1,6 @@
 alert("I am an alert box!");
+
+//I was looking at RegExp a little bit and trying
+//to figure out how to search for the dates.
+//This is what I could come up with for it.
+var testDate = /[0-9][0-9]\W[0-9][0-9]\s[AMP]/g;
