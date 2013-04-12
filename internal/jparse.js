5b0b36e6b03125a90b5bad319c9f916478283213
Wil Gunter <in.wil.i.trust@gmailcom>
2013-04-12 00:10:59 -0700

    Fix tard math

--- internal/jparse.js
+++ internal/jparse.js
@@ -6,7 +6,7 @@ var firstDate = 00;
 var month = 0;
 var parse = null;
 var parseArray = [];
-var daysPerMonth = [00, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+var daysPerMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 function Date(subject,startDate, startTime, endDate, endTime,allDayEvent,description,location,boolPrivate){
 	this.subject = subject;
@@ -22,17 +22,19 @@ function Date(subject,startDate, startTime, endDate, endTime,allDayEvent,descrip
 
 var formatDates = function(){
 	/* This is something i will not be able to do without the website being up
-	the plan will be to grab things peice by peice and sort them into objects.
+	the plan will be to grab things peice by piece and sort them into objects.
 	will want to create the object via day date for now( may cause problems when
 	we get splitting to second month.*/
 }
 
 var toArray = function (){
+	// (0111:00AM-07:00PML-000359-DEPT50700)
+	console.log("Length of parse: " + parse.length + " number of work days: " + (parse.length / 35));
+	
 	try{
 		for(var i = 0; i < (parse.length / 35); i++){
-		// (0111:00AM-07:00PML-000359-DEPT50700)
-			parseArray[i] = parse.substring((i * 35), 35);
-			console.log("debug - creating array");
+			parseArray[i] = parse.substring((i * 35), ((i * 35) + 35));
+			console.log("split portion at coords: " + (i * 35)+ ", "+ ((i * 35) + 35) + " with a result of: " + parse.substring((i * 35), ((i * 35) + 35)));
 		}
 	}catch(err){
 		console.log("Error in creating array, please check for accuracy");
@@ -62,40 +64,40 @@ var initDates = function(){
 var getMonth = function(){
 	switch($('span.calMonthTitle').text()){
 		case "January":
-			month = 1;
+			month = 0;
 			break;
 		case "February":
-			month = 2;
+			month = 1;
 			break;
 		case "March":
-			month = 3;
+			month = 2;
 			break;
 		case "April":
-			month = 4;
+			month = 3;
 			break;
 		case "May":
-			month = 5;
+			month = 4;
 			break;
 		case "June":
-			month = 6;
+			month = 5;
 			break;
 		case "July":
-			month = 7;
+			month = 6;
 			break;
 		case "August":
-			month = 8;
+			month = 7;
 			break;
 		case "September":
-			month = 9;
+			month = 8;
 			break;
 		case "October":
-			month = 10;
+			month = 9;
 			break;
 		case "November":
-			month = 11;
+			month = 10;
 			break;
 		case "December":
-			month = 12;
+			month = 11;
 			break;
 		default:
 			break;
