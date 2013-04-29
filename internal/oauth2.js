900be0ed270b979f0126bed4964fbaa5ca02d630
David King <dxking16@gmail.com>
2013-04-28 18:48:29 -0700

    getting confused/started with oath

---
+++ internal/oauth2.js
@@ -0,0 +1,5 @@
+var googleAuth = new OAuth2('google', {
+	client_id: '981901574050.apps.googleusercontent.com'
+	client_secret: 'AI-p5AEWFdpSq9p7PLjGy9TK'
+	api_scope: 'https://www.googleapis.com/auth/calendar'
+});
