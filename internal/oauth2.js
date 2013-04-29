b955e3517379139727c8334bc65976fe46a56922
David King <dxking16@gmail.com>
2013-04-28 18:54:10 -0700

    more stuff?

--- internal/oauth2.js
+++ internal/oauth2.js
@@ -1,5 +1,10 @@
+// http://smus.com/oauth2-chrome-extensions/ is where i'm finding this info
+
 var googleAuth = new OAuth2('google', {
 	client_id: '981901574050.apps.googleusercontent.com'
 	client_secret: 'AI-p5AEWFdpSq9p7PLjGy9TK'
 	api_scope: 'https://www.googleapis.com/auth/calendar'
+	
+	//redirect_uri: 'https://mytlc.bestbuy.com/etm/time/timesheet/etmTnsMonth.jsp' not sure if this is needed, or if it's even the correct place
+	//javascript_origins: 'https://mytlc.bestbuy.com' same as above
 });
